e76d59a6eae0d7dce4fc31e4df1518dbbacec8ee
Elias Hernandez <ehernandez@evistaresources.com>
2020-12-23 18:43:26 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,9 +35,11 @@ export default function Home() {
             <a className='button' href='/snapchat'>
               Snapchat
             </a>
+            <!--
             <a className='button' href='/youtube'>
               YouTube
             </a>
+            -->
           </div>
         </div>
       </main>
