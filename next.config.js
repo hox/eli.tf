e8641c9dc6550f8f6440fa9d5309d20a6a9e6bd2
Elias Hernandez <ehernandez@evistaresources.com>
2021-05-26 02:09:37 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,21 +10,9 @@ module.exports = {
         destination: 'https://go.eli.tf/github',
       },
       {
-        source: '/discord',
-        destination: 'https://go.eli.tf/discord',
-      },
-      {
-        source: '/twitch',
-        destination: 'https://go.eli.tf/twitch',
-      },
-      {
         source: '/snapchat',
         destination: 'https://go.eli.tf/snapchat',
-      },
-      {
-        source: '/youtube',
-        destination: 'https://go.eli.tf/youtube',
-      },
+      }
     ];
   },
 };
