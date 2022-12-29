72308517c4279a509436cc5e68a52c9c1fce26a8
Elias Hernandez <eli@socials.link>
2022-12-28 22:06:20 -0500

    lint

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,14 +2,14 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/_app.tsx";
-import * as $1 from "./routes/index.tsx";
+import config from './deno.json' assert { type: 'json' };
+import * as $0 from './routes/_app.tsx';
+import * as $1 from './routes/index.tsx';
 
 const manifest = {
   routes: {
-    "./routes/_app.tsx": $0,
-    "./routes/index.tsx": $1,
+    './routes/_app.tsx': $0,
+    './routes/index.tsx': $1,
   },
   islands: {},
   baseUrl: import.meta.url,
